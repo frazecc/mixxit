72e8047bf0e88e3c28ba71f8eb04c29352bda278
frazecc <fra.zecc@gmail.com>
2026-09-21 08:15:46 +0200

    Add files via upload

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,6 @@
-d
+window.PENOMBRA_CONFIG = {
+  SUPABASE_URL: 'https://ausmfrvlplscchkvkoow.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_6oPe_3N2IHSAlhxPci0yzQ_dU7IbhpQ',
+  APP_NAME: 'La Partita della Penombra',
+  APP_VERSION: 'v0.1.0'
+};
